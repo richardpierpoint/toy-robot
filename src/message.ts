5aa7ce164c9fc89804a9687844e9b68a851e98e3
Richard Pierpoint <richard.pierpoint@gmail.com>
2022-05-04 22:11:20 +0200

    fix message example

--- src/message.ts
+++ src/message.ts
@@ -2,7 +2,7 @@ export const printMessage = () => {
     console.log('Robot Simulator');
     console.log('===============');
     console.log('The following commands are supported: \n\n');
-    console.log('- PLACE x,y,f\nPlaces the robot at the given coordinates and facing the given direction. eg: PLACE 1,3,N\nThis command needs to be executed first\n');
+    console.log('- PLACE x,y,f\nPlaces the robot at the given coordinates and facing the given direction. eg: PLACE 1,3,NORTH\nThis command needs to be executed first\n');
     console.log('- LEFT\nRotates the robot to left\n');
     console.log('- RIGHT\nRotates the robot to right\n');
     console.log('- MOVE\nMoves the robot one place in the direction it is facing\n');
